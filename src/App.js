a3e2797046bfdf49a4d3fe333a591cc9a48bc873
David Beltran <davidalfonsobeltran5@gmail.com>
2021-08-19 00:20:19 -0500

    about us page added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router,Route ,Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 function App() {
-  return (
-    <div className="App">
-      <head>
-        <title>        
-        {document.title='SmartTracking'}
-        </title>
-      </head> 
-      
-      <header className="App-header">
+    return ( <div className = "App" >
+        <head >
+          <title>
+           { document.title = 'SmartTracking' } 
+           </title> 
+        </head> 
+
+        <header className = "App-header" >
+        <img src = { logo } className = "App-logo" alt = "logo"/>
+        &nbsp;&nbsp;
         <h1>
-          Smart Tracking
-        </h1>
-      </header>
-      <Router>
-          <nav className="nav-bar">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/Maps">Maps</Link></li>
-                <li><Link to="/Log">Log</Link></li>
-                <li><Link to="/About_us">About us</Link></li>
-            </ul>
-          </nav>
-          <Route exact path="/"><p>hello</p></Route>
-          <Route path="/Maps"><p>help</p></Route>
-          <Route path="/Log"><p>help</p></Route>
-          <Route path="/About_us"><p>help</p></Route>
+        Smart Tracking 
+        </h1> 
+        </header> 
+        <Router>
+        <nav className = "nav-bar">
+          <ul>
+              <li> < Link to = "/"> Home </Link></li>
+              <li> < Link to = "/Maps"> Maps </Link></li>
+              <li> < Link to = "/Log" > Log </Link></li >
+              <li> < Link to = "/About_us" > About us </Link></li >
+          </ul> 
+        </nav> 
+        <Route exact path = "/" >
+          <p> hello </p> 
+        </Route> 
+        <Route path = "/Maps" >
+          <p> help </p> 
+        </Route> 
+          <Route path = "/Log" >
+        <p > help </p> 
+          </Route> 
+        <Route path = "/About_us" >
+          <div className="backsquater">
+          <p> Este proyecto esta elaborado por los estudiantes tlataltal con el fin 
+            de visualizar los datos de la ubicación geografica enviados desde los dispositivos gps</p> 
+          
+          
+          
+          
+          
+          </div>
+        </Route>
+
+
       </Router>
-        
 
 
 
-    </div>
-  );
-}
 
-export default App;
+            </div>
+        );
+    }
+
+    export default App;
