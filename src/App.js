176c6b4e1a0fbac7d4672608320912655f2e9d7f
David Beltran <davidalfonsobeltran5@gmail.com>
2021-08-18 22:17:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router,Route ,Link} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <head>
+        <title>        
+        {document.title='Pruea'}
+        </title>
+      </head> 
+      
+      <header>
+        <h1>
+          Smart Tracking
+        </h1>
       </header>
+      <Router>
+          <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Maps">Maps</Link></li>
+                <li><Link to="/Log">Log</Link></li>
+                <li><Link to="/About_us">About us</Link></li>
+            </ul>
+          </nav>
+          <Route exact path="/"><p>hello</p></Route>
+          <Route path="/Maps"><p>help</p></Route>
+          <Route path="/Log"><p>help</p></Route>
+          <Route path="/About_us"><p>help</p></Route>
+      </Router>
+        
+
+
+
     </div>
   );
 }
