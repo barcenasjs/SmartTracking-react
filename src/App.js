72e3e060de8e54dda4d3eb0fbcf7685b1c214ab1
David Beltran <davidalfonsobeltran5@gmail.com>
2021-08-18 22:57:05 -0500

    commit no 3

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
         </title>
       </head> 
       
-      <header>
+      <header className="App-header">
         <h1>
           Smart Tracking
         </h1>
       </header>
       <Router>
-          <nav>
+          <nav className="nav-bar">
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/Maps">Maps</Link></li>
