a6db0e3eeac66e920aeff6015e51901da833dc8c
David Beltran <davidalfonsobeltran5@gmail.com>
2021-09-04 09:56:57 -0500

    Map react-google-api implementation

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,19 +1,54 @@
-import React, { Component } from 'react'
+import React,{ useState} from 'react'
 import "./Map.css"
+import  { GoogleMap, useJsApiLoader ,Marker,InfoWindow} from '@react-google-maps/api';
 
-export default class Map extends Component {
-    render(props) {
-        return (
-            <div>
-                <iframe 
-                className="mapa" 
-                title="mapa" 
-                frameborder="0" 
-                src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAjxpJcSdf7dnbP8rj6bPFku1uFUHgNwco&q="+this.props.data._geoloc.lat+","+this.props.data._geoloc.lng+"&zoom=15"}
-                allowfullscreen>
+export default function Map(props) {
+    
+    const [infoWindowVisible, setInfoWindowVisible] = useState(false);
+    const center = {
+        lat: props.data._geoloc.lat,
+        lng: props.data._geoloc.lng
+      };
+    
+    const { isLoaded } = useJsApiLoader({
+      id: 'google-map-script',
+      googleMapsApiKey: "AIzaSyAjxpJcSdf7dnbP8rj6bPFku1uFUHgNwco"
+    })
+   
+    
+      
+    
+  
+    return isLoaded ? (
+        
+        <GoogleMap
+          mapContainerClassName="mapa"
+          center={center}
+          zoom={17}
+          id="map"
+        >
+            <Marker
+                title={center.lat+","+center.lng}
+                position={center}
+                onClick={() =>{setInfoWindowVisible(true)}}
+                clickable={true}
+                animation="DROP"
+            >
+                {infoWindowVisible&&(<InfoWindow
+                    anchor={new window.google.maps.Point(0,-100)}
+                    onCloseClick={() =>{setInfoWindowVisible(false)}}
+                    >
+                        <div className="pop"><p>{center.lat+","+center.lng}</p></div>
 
-                </iframe>
-            </div>
-        )
-    }
-}
+                </InfoWindow>)}
+            </Marker>  
+             <></>
+        </GoogleMap>
+        
+    ) : <></>
+  }
+
+
+
+           
+               
