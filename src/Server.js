07e0595c2ca73d2f901278d9644b187fd4eea45e
Jose Santiago <santiagojd@uninorte.edu.co>
2021-09-13 17:10:26 -0500

    AWS Ip

--- src/Server.js
+++ src/Server.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 export const on = (cb) => {
-    const connection = io('http://bysj.servegame.com:3030');
+    const connection = io('ec2-18-223-136-40.us-east-2.compute.amazonaws.com');
     connection.on('connect_error', err => {
         console.log(`connect_error due to ${err.message}`);
     });
