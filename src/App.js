a65f80ca98af5138b4a6b5f7a26bdb6f6fb17f22
Sbarcenas01 <sbarcenas_@outlook.com>
2021-09-14 10:58:45 -0500

    Saving

--- src/App.js
+++ src/App.js
@@ -1,67 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import Box from './Components/Box';
-import React, {useEffect, useState} from 'react';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
-import {on} from "./Server";
+import Box from "./Components/Box";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { on } from "./Server";
 import moment from "moment";
 function App() {
-    const [state, setState] = useState({
-        _geoloc:{
-            lat:'', lng:'', hora:'', fecha:''
-        }
-    })
+  const [state, setState] = useState({
+    _geoloc: {
+      lat: "",
+      lng: "",
+      hora: "",
+      fecha: "",
+    },
+  });
 
-    useEffect(() => {
-        on((connection) => (geoData) => {
-            const date= new Date();
-            geoData._geoloc["hora"]= moment(date).format("HH:mm:ss");
-            geoData._geoloc["fecha"]= moment(date).format("DD-MM-YYYY")
-            setState(geoData); // ----> data
-            
-        })
-    }, []);
+  useEffect(() => {
+    on((connection) => (geoData) => {
+      console.log(JSON.stringify(geoData));
+      const date = new Date();
+      geoData._geoloc["hora"] = moment(date).format("HH:mm:ss");
+      geoData._geoloc["fecha"] = moment(date).format("DD-MM-YYYY");
+      setState(geoData); // ----> data
+    });
+  }, []);
 
-    return (
-        <div className="App">
-            <head>
-                <title>
-                    SmartTracking
-                </title>
-            </head>
+  return (
+    <div className="App">
+      <head>
+        <title>SmartTracking</title>
+      </head>
 
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                &nbsp;&nbsp;
-                <h1>
-                    Smart Tracking
-                </h1>
-            </header>
-            <Router>
-                <nav className="nav-bar">
-                    <ul>
-                        <li>< Link className="menu" to="/"> Home </Link></li>
-                        <li>< Link className="menu" to="/Maps"> Maps </Link></li>
-                        <li>< Link className="menu" to="/Log"> Log </Link></li>
-                        <li>< Link className="menu" to="/About_us"> About us </Link></li>
-                    </ul>
-                </nav>
-                <Route exact path="/">
-                    <Box contenido="Home" data={state}></Box>
-                </Route>
-                <Route path="/Maps">
-                    <Box contenido="Maps" data={state}></Box>
-                </Route>
-                <Route path="/Log">
-                    <Box contenido="Log" data={state}></Box>
-                </Route>
-                <Route path="/About_us">
-                    <Box contenido="About us" data={state}></Box>
-                </Route>
-            </Router>
-        </div>
-    )
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        &nbsp;&nbsp;
+        <h1>Smart Tracking</h1>
+      </header>
+      <Router>
+        <nav className="nav-bar">
+          <ul>
+            <li>
+              <Link className="menu" to="/">
+                {" "}
+                Home{" "}
+              </Link>
+            </li>
+            <li>
+              <Link className="menu" to="/Maps">
+                {" "}
+                Maps{" "}
+              </Link>
+            </li>
+            <li>
+              <Link className="menu" to="/Log">
+                {" "}
+                Log{" "}
+              </Link>
+            </li>
+            <li>
+              <Link className="menu" to="/About_us">
+                {" "}
+                About us{" "}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Route exact path="/">
+          <Box contenido="Home" data={state}></Box>
+        </Route>
+        <Route path="/Maps">
+          <Box contenido="Maps" data={state}></Box>
+        </Route>
+        <Route path="/Log">
+          <Box contenido="Log" data={state}></Box>
+        </Route>
+        <Route path="/About_us">
+          <Box contenido="About us" data={state}></Box>
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Server.js
+++ src/Server.js
@@ -1,9 +1,9 @@
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 export const on = (cb) => {
-    const connection = io('http://bysj.servegame.com:3030');
-    connection.on('connect_error', err => {
-        console.log(`connect_error due to ${err.message}`);
-    });
-    connection.on('tcpudp created', cb(connection));
+  const connection = io("https://santi-tracking-api.bahoque.com/");
+  connection.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+  connection.on("tcpudp created", cb(connection));
 };
