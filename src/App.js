f9572f72c45865ef19190267252caf7d30249921
David Beltran <davidalfonsobeltran5@gmail.com>
2021-09-20 16:16:01 -0500

    Merging

--- src/App.js
+++ src/App.js
@@ -7,79 +7,60 @@ import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import { on } from "./Server";
 import moment from "moment";
 function App() {
-  const [state, setState] = useState({
-    _geoloc: {
-      lat: "",
-      lng: "",
-      hora: "",
-      fecha: "",
-    },
-  });
-  useEffect(() => {
-    on((connection) => (geoData) => {
-      const date = new Date();
-      const parseData = JSON.parse(geoData.position);
-      parseData._geoloc["hora"] = moment(date).format("HH:mm:ss");
-      parseData._geoloc["fecha"] = moment(date).format("DD-MM-YYYY");
-      setState(parseData); // ----> data});
-    });
-  }, []);
+    const [Data, setData] = useState([])
 
-  return (
-    <div className="App">
-      <head>
-        <title>SmartTracking</title>
-      </head>
+    useEffect(() => {
+        on((connection) => (geoData) => {
+            const date= new Date();
+            geoData._geoloc["hora"]= moment(date).format("HH:mm:ss");
+            geoData._geoloc["fecha"]= moment(date).format("DD-MM-YYYY")
+            setData([...Data,geoData]); // ----> data
+            
+        })
+    }, []);
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        &nbsp;&nbsp;
-        <h1>Smart Tracking</h1>
-      </header>
-      <Router>
-        <nav className="nav-bar">
-          <ul>
-            <li>
-              <Link className="menu" to="/">
-                {" "}
-                Home{" "}
-              </Link>
-            </li>
-            <li>
-              <Link className="menu" to="/Maps">
-                {" "}
-                Maps{" "}
-              </Link>
-            </li>
-            <li>
-              <Link className="menu" to="/Log">
-                {" "}
-                Log{" "}
-              </Link>
-            </li>
-            <li>
-              <Link className="menu" to="/About_us">
-                {" "}
-                About us{" "}
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <Route exact path="/">
-          <Box contenido="Home" data={state}></Box>
-        </Route>
-        <Route path="/Maps">
-          <Box contenido="Maps" data={state}></Box>
-        </Route>
-        <Route path="/Log">
-          <Box contenido="Log" data={state}></Box>
-        </Route>
-        <Route path="/About_us">
-          <Box contenido="About us" data={state}></Box>
-        </Route>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <head>
+                <title>
+                    SmartTracking
+                </title>
+            </head>
+
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                &nbsp;&nbsp;
+                <h1>
+                    Smart Tracking
+                </h1>
+            </header>
+            <Router>
+                <nav className="nav-bar">
+                    <ul>
+                        <li>< Link className="menu" to="/"> Home </Link></li>
+                        <li>< Link className="menu" to="/Maps"> Maps </Link></li>
+                        
+                        <li>< Link className="menu" to="/About_us"> About us </Link></li>
+                    </ul>
+                </nav>
+                <Route exact path="/">
+                    <Box contenido="Home" data={Data}></Box>
+                </Route>
+                <Route path="/Maps">
+                    <Box contenido="Maps" data={Data}></Box>
+                </Route>
+                
+                <Route path="/About_us">
+                    <Box contenido="About us" data={Data}></Box>
+                </Route>
+            </Router>
+            <button
+            onClick={()=>{console.log(Data)}}
+            >
+                <p>hello</p>
+            </button>
+        </div>
+    )
 }
 
 export default App;
