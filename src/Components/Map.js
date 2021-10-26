2dca627d7b3c95a94687a74ad8c08f063819b23b
Sebastian Barcenas <sbarcenas00@gmail.com>
2021-10-26 03:01:00 -0500

    Updating data retriveer

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -24,14 +24,16 @@ export default function Map(props) {
 
   //new data
   const [dataCar1, setDataCar1] = useState([]);
+  const [historyCountCar1, setHistoryCountCar1] = useState([]);
   const [dataCar2, setDataCar2] = useState([]);
-  console.log("hola" + JSON.stringify(dataCar1[dataCar1.length - 1]));
+  const [historyCountCar2, setHistoryCountCar2] = useState([]);
+
+  console.log("soy data 2" + JSON.stringify(dataCar2));
   const { Option } = Select;
 
   function handleChange(value) {
     console.log(`selected ${value}`);
   }
-  
 
   useEffect(() => {
     position
@@ -45,11 +47,11 @@ export default function Map(props) {
             return { lat: parseFloat(el.lat), lng: parseFloat(el.lng) };
           });
         const car2 = res.data
-          .filter((el) => el.user_id === 2)
+          .filter((el) => el.user_id === 9)
           .map((el) => {
             return { lat: parseFloat(el.lat), lng: parseFloat(el.lng) };
           });
-
+        console.log(JSON.stringify(res));
         setDataCar1(car1);
         setDataCar2(car2);
       })
@@ -71,18 +73,75 @@ export default function Map(props) {
               return { lat: parseFloat(el.lat), lng: parseFloat(el.lng) };
             });
           const car2 = res.data
-            .filter((el) => el.user_id === 2)
+            .filter((el) => el.user_id === 9)
             .map((el) => {
               return { lat: parseFloat(el.lat), lng: parseFloat(el.lng) };
             });
-          setDataCar1(car1);
-          setDataCar2(car2);
+          setHistoryCountCar1(car1[car1.length - 1]);
+          setHistoryCountCar2(car2[car2.length - 1]);
         })
         .catch((e) => {
           alert(e);
         });
     });
   }, []);
+
+  useEffect(() => {
+    if (dataCar2[0].lng && dataCar1[0].lng) {
+      alert("hola");
+      if (Range.length !== 0) {
+        const positions1 = dataCar1
+          .map((el) => {
+            return {
+              lat: el.lat,
+              lng: el.lng,
+              date: el.date,
+            };
+          })
+          .filter((el) => {
+            return new Date(Range[0]).getTime() < new Date(el.date).getTime();
+          })
+          .filter((el) => {
+            return new Date(el.date).getTime() < new Date(Range[1]).getTime();
+          });
+
+        const positions2 = dataCar2
+          .map((el) => {
+            return {
+              lat: el.lat,
+              lng: el.lng,
+              date: el.date,
+            };
+          })
+          .filter((el) => {
+            return new Date(Range[0]).getTime() < new Date(el.date).getTime();
+          })
+          .filter((el) => {
+            return new Date(el.date).getTime() < new Date(Range[1]).getTime();
+          });
+        alert(JSON.stringify(positions1[0]));
+        setDataCar1(positions1);
+        setDataCar2(positions2);
+      } else {
+        const positions1 = props.data.map((el) => {
+          return {
+            lat: el.lat,
+            lng: el.lng,
+            date: el.date,
+          };
+        });
+        const positions2 = props.data.map((el) => {
+          return {
+            lat: el.lat,
+            lng: el.lng,
+            date: el.date,
+          };
+        });
+        setDataCar2(positions2);
+        setDataCar1(positions1);
+      }
+    }
+  }, [Range, dataCar1, dataCar2]);
 
   useEffect(() => {
     if (props?.data[0]?.position && historyCount.length === 0) {
@@ -174,16 +233,23 @@ export default function Map(props) {
 
   return isLoaded ? (
     <>
-    <Row gutter={[16, 24]}>
-          <Col> <b></b> </Col>
-          <Col className="gutter-row" span={24}>
-            <Select defaultValue="Vehículo 1" style={{ width: 120 }} onChange={handleChange}>
-              <Option value="Vehículo 1">Vehículo 1</Option>
-              <Option value="Vehículo 2">Vehículo 2</Option>
-              <Option value="Ambos">Ambos</Option>  
-            </Select> 
-          </Col>
-    </Row>
+      <Row gutter={[16, 24]}>
+        <Col>
+          {" "}
+          <b></b>{" "}
+        </Col>
+        <Col className="gutter-row" span={24}>
+          <Select
+            defaultValue="Vehículo 1"
+            style={{ width: 120 }}
+            onChange={handleChange}
+          >
+            <Option value="Vehículo 1">Vehículo 1</Option>
+            <Option value="Vehículo 2">Vehículo 2</Option>
+            <Option value="Ambos">Ambos</Option>
+          </Select>
+        </Col>
+      </Row>
       <GoogleMap
         mapContainerClassName="mapa"
         center={
@@ -199,11 +265,59 @@ export default function Map(props) {
           position={
             history
               ? PollyneData[PollyneData.length - 1]
+              : dataCar2[dataCar1.length - 1]
+          }
+          clickable
+          onClick={() => {
+            setMarkerInfo(!markerInfo);
+          }}
+        >
+          {markerInfo ? (
+            <InfoWindow
+              position={
+                history
+                  ? PollyneData[PollyneData.length - 1]
+                  : dataCar1[dataCar1.length - 1]
+              }
+            >
+              <div>
+                <h3>Vehículo 1</h3>
+
+                <p>
+                  {history
+                    ? "Lng: " + PollyneData[PollyneData.length - 1].lng
+                    : "Lng: " + historyCount[historyCount.length - 1].lng}
+                </p>
+                <p>
+                  {history
+                    ? "Lat: " + PollyneData[PollyneData.length - 1].lat
+                    : "Lat: " + historyCount[historyCount.length - 1].lat}
+                </p>
+                <p>
+                  {history
+                    ? "Hora: " + PollyneData[PollyneData.length - 1].date
+                    : "Hora: " + historyCount[historyCount.length - 1].date}
+                </p>
+                <p>
+                  {history
+                    ? "Fecha: " + PollyneData[PollyneData.length - 1].date
+                    : "Fecha: " + historyCount[historyCount.length - 1].date}
+                </p>
+              </div>
+            </InfoWindow>
+          ) : null}
+        </Marker>
+
+        <Marker
+          icon={imagen}
+          position={
+            history
+              ? PollyneData[PollyneData.length - 1]
               : dataCar1[dataCar1.length - 1]
           }
           clickable
-          onClick={()=> {
-            setMarkerInfo(!markerInfo)
+          onClick={() => {
+            setMarkerInfo(!markerInfo);
           }}
         >
           {markerInfo ? (
@@ -276,7 +390,6 @@ export default function Map(props) {
             zIndex: 1,
           }}
         ></Polyline>
-
       </GoogleMap>
     </>
   ) : (
